21240afbdeff6c642ff32dcbcdf2baa58a38eadc
brajesh <bk04031997@gmail.com>
2020-05-10 22:47:18 +0530

    initial setup

---
+++ spring-zipkin/src/main/java/com/java/app/SpringZipkinApplication.java
@@ -0,0 +1,27 @@
+package com.java.app;
+
+import org.slf4j.Logger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringZipkinApplication {
+
+	private static final Logger log=org.slf4j.LoggerFactory.getLogger(SpringZipkinApplication.class);
+	
+			@GetMapping("/gettext")
+	public String getText()
+	{
+				log.info("Called");
+		return "Hello";
+	}
+	
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringZipkinApplication.class, args);
+	}
+
+}
---
+++ spring-zipkin/src/main/java/com/java/app/config/CloudConfig.java
@@ -0,0 +1,23 @@
+package com.java.app.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
+@Configuration
+public class CloudConfig {
+	 @Autowired
+	    RestTemplate restTemplate;
+	@Bean
+	public AlwaysSampler defaultSampler()
+	{
+		return new AlwaysSampler();
+	}
+	
+	 
+	    @Bean
+	    public RestTemplate getRestTemplate() {
+	        return new RestTemplate();
+	    }
+}
